7b975f93aa05d4c6a5adbde62662a58760bf7880
Dedovic, Nermin <dedovicnermin@outlook.com>
2023-08-20 16:23:51 -0500

    refactor config extraction / test

--- src/main/java/io/nermdev/kafka/stateful_rebalance_client/util/ConfigExtractor.java
+++ src/main/java/io/nermdev/kafka/stateful_rebalance_client/util/ConfigExtractor.java
@@ -1,66 +1,56 @@
 package io.nermdev.kafka.stateful_rebalance_client.util;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public final class ConfigExtractor {
-    static final String CONSUMER_PREFIX="consumers.";
-    static final String PRODUCER_PREFIX="producers.";
     static final String INTERCEPTOR_CLASSES = "interceptor.classes";
-
 
     private ConfigExtractor() {}
 
-    public static Map<String, Object> extractConfig(final Map<String, Object> config, final AppClientType clientType, final String clientPrefix) {
+    public static Map<String, Object> extractConfig(final Map<String, Object> config, final AppClientType type) {
         Map<String, Object> clientConfig = new HashMap<>();
-        List<Map.Entry<String, Object>> targetClientConfig;
-        if (clientType == AppClientType.CONSUMER) {
-            targetClientConfig = extractClientConfig(config, CONSUMER_PREFIX, clientPrefix);
-        } else {
-            targetClientConfig = extractClientConfig(config, PRODUCER_PREFIX, clientPrefix);
+        List<Map.Entry<String, Object>> targetClientConfig = extractClientConfig(config, type);
+        targetClientConfig.forEach(
+                entry -> clientConfig.put(entry.getKey().substring(type.getPrefix().length() + 1), entry.getValue())
+        );
+        return configureInterceptors(clientConfig);
+    }
+
+    private static Map<String, Object> configureInterceptors(final Map<String, Object> filteredConfig) {
+        final Object value = filteredConfig.get(INTERCEPTOR_CLASSES);
+        final List<? extends Class<?>> classes = Optional.ofNullable(value)
+                .map(v -> (String) v)
+                .map(stringClasses -> Arrays.stream(stringClasses.split(",")).collect(Collectors.toList()))
+                .map(stringClasses -> stringClasses.stream().map(ConfigExtractor::classForName).collect(Collectors.toList()))
+                .orElseGet(ArrayList::new);
+        return Optional.of(classes)
+                .filter(cs -> !cs.isEmpty())
+                .map(cs -> {
+                    filteredConfig.put(INTERCEPTOR_CLASSES, cs);
+                    return filteredConfig;
+                }).orElse(filteredConfig);
+    }
+
+    private static Class<?> classForName(String name) {
+        try {
+            return Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            return null;
         }
 
-
-
-        targetClientConfig.forEach(
-                entry -> clientConfig.put(entry.getKey().substring(CONSUMER_PREFIX.length() + clientPrefix.length() + 1), entry.getValue())
-        );
-
-        Optional.ofNullable((String)clientConfig.get(INTERCEPTOR_CLASSES))
-                .ifPresent(s -> {
-                    final List<String> collect = Arrays.stream(s.split(",")).collect(Collectors.toList());
-                    if (collect.size() > 1) {
-                        try {
-                            clientConfig.put(INTERCEPTOR_CLASSES, Arrays.asList(Class.forName(collect.get(0)), Class.forName(collect.get(1))));
-                        } catch (ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    } else {
-                        try {
-                            clientConfig.put(INTERCEPTOR_CLASSES, Collections.singletonList(Class.forName(collect.get(0))));
-                        } catch (ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-
-        return clientConfig;
     }
 
-
-
-    private static List<Map.Entry<String, Object>> extractClientConfig(final Map<String, Object> config, final String basePrefix, final String clientPrefix) {
+    private static List<Map.Entry<String, Object>> extractClientConfig(final Map<String, Object> config, final AppClientType type) {
         return config.entrySet().stream()
-                .filter(entry -> entry.getKey().startsWith(basePrefix + clientPrefix))
+                .filter(entry -> entry.getKey().startsWith(type.getPrefix()))
                 .collect(Collectors.toList());
     }
-
-
-
-
-
-
-
 
 }
--- src/test/java/io/nermdev/kafka/stateful_rebalance_client/util/ConfigExtractorTest.java
+++ src/test/java/io/nermdev/kafka/stateful_rebalance_client/util/ConfigExtractorTest.java
@@ -2,40 +2,36 @@ package io.nermdev.kafka.stateful_rebalance_client.util;
 
 import io.nermdev.kafka.stateful_rebalance_client.Application;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigExtractorTest {
 
-    @Test
-    void testExtractPlayers() {
-        final Properties method = method();
-        final Map<String, Object> stringObjectMap = Application.propertiesToMap(method);
+    Properties appConfig;
 
+    @ParameterizedTest
+    @EnumSource(value = AppClientType.class, names = {"CONSUMER_PLAYER", "CONSUMER_SCORE", "CONSUMER_PRODUCT", "PRODUCER_SCORECARD"})
+    void testExtractPlayers(final AppClientType clientType) {
+        System.out.println(clientType.name());
+        appConfig = Optional.ofNullable(appConfig)
+                .orElse(method());
+        final Map<String, Object> stringObjectMap = Application.propertiesToMap(appConfig);
 
-        final Map<String, Object> actual = ConfigExtractor.extractConfig(stringObjectMap, AppClientType.CONSUMER,"player");
+        final Map<String, Object> actual = ConfigExtractor.extractConfig(stringObjectMap, clientType);
         Assertions.assertThat(actual).isNotNull();
         actual.keySet().stream().sorted()
-                .forEach(key -> {
-                    System.out.println(key + " : " + actual.get(key));
-                });
-
-
-
-
+                .forEach(key -> System.out.println(key + " : " + actual.get(key)));
+        final String suffix = clientType.name().toLowerCase().split("_")[1];
+        Assertions.assertThat((String)actual.get("client.id")).isNotNull().contains(suffix);
     }
-
-
-
-
 
     private Properties method() {
         final Properties properties = new Properties();
